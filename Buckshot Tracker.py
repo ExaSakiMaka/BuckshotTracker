c74fefa2b07dc3d5e7341b87e39cb5e09cb4a0f1
ExaSakiMaka <72074670+ExaSakiMaka@users.noreply.github.com>
2024-12-08 22:44:30 +0100

    Update Buckshot Tracker.py
    added comments to the code

--- Buckshot Tracker.py
+++ Buckshot Tracker.py
@@ -1,45 +1,46 @@
-import os
+import os #importing os module to clear the screen
 
-Balles = []
-Current = "?"
 Loop = 1
-FirstTurn = 1
+FirstTurn = 1 #initializing Basic Variables
+
     
 
 while Loop == 1:
     if FirstTurn == 1:
         os.system('cls')
-        blue =int(input("Blank : "))
-        red =int(input("Live : "))
-        numballes = int(blue) + int(red)
-        Balles =["?"]*numballes
+        Blanks =int(input("Blank : ")) #Number of Blanks
+        Lives =int(input("Live : ")) #Number of Lives
+        numballes = int(Blanks) + int(Lives) #Calculating Number of Bullets
+        Balles =["?"]*numballes #Initializing the list
         FirstTurn = 0
         os.system('cls')
     
-    if red == 0:
+    if Lives == 0: #If no lives left fill the list with blanks
         Balles =["B"]*numballes
-    elif blue == 0:
+    elif Blanks == 0: #if no blanks left fill the list with lives
         Balles =["L"]*numballes
         
-    if list.count(Balles,"L") == red:
+    if list.count(Balles,"L") == Lives: #If all lives are in the list fill the missing bullets with blanks
         for i in range(len(Balles)):
             if Balles[i] == "?":
                 Balles[i] = "B"
             
-    elif list.count(Balles,"B") == blue:
+    elif list.count(Balles,"B") == Blanks: #If all blanks are in the list fill the missing bullets with lives
         for i in range(len(Balles)):
             if Balles[i] == "?":
                 Balles[i] = "L"
 
-    BlankPercent = round(blue*100/numballes)
-    LivePercent = round(red*100/numballes)
+    #Calculating percentages
+    BlankPercent = round(Blanks*100/numballes)
+    LivePercent = round(Lives*100/numballes)
     
-    print("",blue,"Blank\n",red,"Live\n",numballes,"Balles\n\nCurrent :",Balles[0],"\n",Balles,"\n")
+    #Printing the interface
+    print("",Blanks,"Blank\n",Lives,"Live\n",numballes,"Balles\n\nCurrent :",Balles[0],"\n",Balles,"\n")
     print("Next shot :\nBlank :",str(BlankPercent)+"%\nLive : "+str(LivePercent)+"%\n")
     
     todo =input("1 : Phone \n2 : Beer/Shot \n3 : End Round \n4 : Exit \n")
     
-    if todo == "1":
+    if todo == "1": #asks for position and type of the bullet to place it in the list
         os.system('cls')
         Pos = input("Position : ")
         Type = input("Type : ")
@@ -47,24 +48,24 @@ while Loop == 1:
         
     elif todo == "2":
         os.system('cls')
-        "Current :",Balles[0]
+        "Current :",Balles[0] #removes the first bullet
         numballes = numballes - 1
         
-        if Balles[0] == "?":
+        if Balles[0] == "?": #if the shot is unknown asks for the type
             Balles[0] = input("Blank or Live ? (B/L) : ")
             
-        if Balles[0] == "B":
-            blue = blue - 1
+        if Balles[0] == "B": #then updates the list
+            Blanks = Blanks - 1
         elif Balles[0] == "L":
-            red = red - 1
+            Lives = Lives - 1
         del Balles [0]
         
     elif todo == "3":
-        FirstTurn = 1
+        FirstTurn = 1 #restarts the game
     elif todo == "4":
-        Loop = 0
+        Loop = 0 #ends the game
     
-    if Balles == []:
+    if Balles == []: #simple auto restart
         os.system('cls')
         Restart = input("Round Over \nRestart ? (Y/N) : ")
         if Restart == "Y":
